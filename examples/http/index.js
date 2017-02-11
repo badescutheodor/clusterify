66cca0c35dcf8146042fd91d76afdf1530ccc961
Theodor Badescu <Theodor Badescu>
2017-02-11 22:14:47 +0200

    Refactored the http example.

--- examples/http/index.js
+++ examples/http/index.js
@@ -1,5 +1,6 @@
 import clusterify from '../../src'
 import http from 'http'
+import os from 'os'
 
 /**
  * Let's see if we can use the module to load balance
@@ -8,27 +9,28 @@ import http from 'http'
 
 const opts = {
     master: (bridge) => {
-        let workers = [1, 2, 3, 4, 5, 6, 7, 8];
+        let workers = [];
+
+        for(let i = 0, len = os.cpus().length; i < len; i++)
+        {
+            if (i === 0 ) continue;
+            workers.push(i);
+        }
 
         http.createServer((req, res) => {
             let worker = workers.shift();
             workers.push(worker);
 
-            bridge.from(worker).on('response', (data) => {
-                data.ack();
-                res.end(data.getPayload());
+            bridge.to(worker).emit("new:request", null, (replies) => {
+                res.end(replies.get(worker));
             });
-
-            bridge.to(worker).emit("new:request");
 
         }).listen(3000);
     },
 
     workers: (bridge) => {
-        bridge.from(0).on("new:request", () => {
-            bridge.to(0).emit('response', `Worker with id ${bridge.id} says hello :)!`, () => {
-                console.log('Everything\'s okay <3, I served the request.', bridge.id);
-            });
+        bridge.from(0).on("new:request", (data) => {
+            data.ack(`Everything\'s okay <3, I served the request: ${bridge.id}`);
         });
     }
 };
